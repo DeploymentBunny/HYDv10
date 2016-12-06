ca71001d3b2bef765daf466ca6b4518c7d89e95c
Mikael Nystrom <deploymentbunny@outlook.com>
2016-12-06 02:29:51 -0800

    Update

---
+++ TaskSequences/DeployADDS01.ps1
@@ -0,0 +1,126 @@
+﻿Param
+(
+    [parameter(position=0,mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({Test-Path -Path $_})]
+    [string]
+    $SettingsFile = "C:\Setup\FABuilds\FASettings.xml",
+
+    [parameter(Position=1,mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({Test-Path -Path $_})]
+    [String]
+    $VHDImage = "C:\Setup\VHD\WS2016-DCE_UEFI.vhdx",
+    
+    [parameter(Position=2,mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({Test-Path -Path $_})]
+    [String]
+    $VMlocation = "D:\VMs"
+)
+
+#Import Modules
+Import-Module C:\setup\Functions\VIAHypervModule.psm1 -Force
+Import-Module C:\setup\Functions\VIADeployModule.psm1 -Force
+Import-Module C:\Setup\Functions\VIAUtilityModule.psm1 -Force
+
+#Set Values
+$ServerName = "ADDS01"
+$DomainName = "Fabric"
+
+#Read data from XML
+Write-Verbose "Reading $SettingsFile"
+[xml]$Settings = Get-Content $SettingsFile
+$CustomerData = $Settings.FABRIC.Customers.Customer
+$CommonSettingData = $Settings.FABRIC.CommonSettings.CommonSetting
+$ProductKeysData = $Settings.FABRIC.ProductKeys.ProductKey
+$NetworksData = $Settings.FABRIC.Networks.Network
+$DomainData = $Settings.FABRIC.Domains.Domain | Where-Object -Property Name -EQ -Value $DomainName
+$ServerData = $Settings.FABRIC.Servers.Server | Where-Object -Property Name -EQ -Value $ServerName
+$NIC001 = $ServerData.Networkadapters.Networkadapter | Where-Object -Property id -EQ -Value Networkadapter001
+$NIC001RelatedData = $NetworksData | Where-Object -Property ID -EQ -Value $NIC001.ConnectedToNetwork
+
+$MountFolder = "C:\MountVHD"
+$AdminPassword = $CommonSettingData.LocalPassword
+$DomainInstaller = $DomainData.DomainAdmin
+$DomainName = $DomainData.DomainAdminDomain
+$DNSDomain = $DomainData.DNSDomain
+$DomainAdminPassword = $DomainData.DomainAdminPassword
+$VMMemory = [int]$ServerData.Memory * 1024 * 1024
+$VMSwitchName = $CommonSettingData.VMSwitchName
+$localCred = new-object -typename System.Management.Automation.PSCredential -argumentlist "Administrator", (ConvertTo-SecureString $adminPassword -AsPlainText -Force)
+$domainCred = new-object -typename System.Management.Automation.PSCredential -argumentlist "$($domainName)\Administrator", (ConvertTo-SecureString $domainAdminPassword -AsPlainText -Force)
+$VIASetupCompletecmdCommand = "cmd.exe /c PowerShell.exe -Command New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest' -Name OSDeployment -Value Done -PropertyType String"
+$SetupRoot = "C:\Setup"
+
+If ((Test-VIAVMExists -VMname $($ServerData.ComputerName)) -eq $true){Write-Host "$($ServerData.ComputerName) already exist";Break}
+Write-Host "Creating $($ServerData.ComputerName)"
+$VM = New-VIAVM -VMName $($ServerData.ComputerName) -VMMem $VMMemory -VMvCPU 2 -VMLocation $VMLocation -VHDFile $VHDImage -DiskMode Diff -VMSwitchName $VMSwitchName -VMGeneration 2 -Verbose
+$VIAUnattendXML = New-VIAUnattendXML -Computername $($ServerData.ComputerName) -OSDAdapter0IPAddressList $NIC001.IPAddress -DomainOrWorkGroup Workgroup -ProtectYourPC 3 -Verbose -OSDAdapter0Gateways $NIC001RelatedData.Gateway -OSDAdapter0DNS1 $NIC001RelatedData.DNS[0] -OSDAdapter0DNS2 $NIC001RelatedData.DNS[1] -OSDAdapter0SubnetMaskPrefix $NIC001RelatedData.SubNet -OrgName $CustomerData.Name -Fullname $CustomerData.Name -TimeZoneName $CommonSettingData.TimeZoneName
+$VIASetupCompletecmd = New-VIASetupCompleteCMD -Command $VIASetupCompletecmdCommand -Verbose
+$VHDFile = (Get-VMHardDiskDrive -VMName $($ServerData.ComputerName)).Path
+Mount-VIAVHDInFolder -VHDfile $VHDFile -VHDClass UEFI -MountFolder $MountFolder 
+New-Item -Path "$MountFolder\Windows\Panther" -ItemType Directory -Force | Out-Null
+New-Item -Path "$MountFolder\Windows\Setup" -ItemType Directory -Force | Out-Null
+New-Item -Path "$MountFolder\Windows\Setup\Scripts" -ItemType Directory -Force | Out-Null
+Copy-Item -Path $VIAUnattendXML.FullName -Destination "$MountFolder\Windows\Panther\$($VIAUnattendXML.Name)" -Force
+Copy-Item -Path $VIASetupCompletecmd.FullName -Destination "$MountFolder\Windows\Setup\Scripts\$($VIASetupCompletecmd.Name)" -Force
+Copy-Item -Path $SetupRoot\functions -Destination $MountFolder\Setup\Functions -Container -Recurse
+Copy-Item -Path $SetupRoot\HYDV10 -Destination $MountFolder\Setup\HYDV10 -Container -Recurse
+Dismount-VIAVHDInFolder -VHDfile $VHDFile -MountFolder $MountFolder
+Remove-Item -Path $VIAUnattendXML.FullName
+Remove-Item -Path $VIASetupCompletecmd.FullName
+
+#Deploy
+Write-Host "Working on $($ServerData.ComputerName)"
+Start-VM $($ServerData.ComputerName)
+Wait-VIAVMIsRunning -VMname $($ServerData.ComputerName)
+Wait-VIAVMHaveICLoaded -VMname $($ServerData.ComputerName)
+Wait-VIAVMHaveIP -VMname $($ServerData.ComputerName)
+Wait-VIAVMDeployment -VMname $($ServerData.ComputerName)
+Wait-VIAVMHavePSDirect -VMname $($ServerData.ComputerName) -Credentials $localCred
+
+#Add role ADDS
+$Role = "ADDS"
+Invoke-Command -VMName $($ServerData.ComputerName) -ScriptBlock {
+    Param(
+        $Role
+    )
+    C:\Setup\HYDv10\Scripts\Invoke-VIAInstallRoles.ps1 -Role $Role
+} -Credential $localCred -ArgumentList $Role
+
+#Add role ADDS
+$DomainForestLevel = 'ws2016'
+Invoke-Command -VMName $($ServerData.ComputerName) -ScriptBlock {
+    Param(
+        $Password,
+        $FQDN,
+        $NetBiosDomainName,
+        $DomainForestLevel
+    )
+    C:\Setup\HYDv10\Scripts\Set-FARole-ADDS-FDC.ps1 -Password $Password -FQDN $FQDN -NetBiosDomainName $NetBiosDomainName -DomainForestLevel $DomainForestLevel
+} -Credential $localCred -ArgumentList $DomainData.DomainAdminPassword,$DomainData.DNSDomain,$DomainData.DomainNetBios,$DomainForestLevel
+
+#Restart
+Restart-VIAVM -VMname $($ServerData.ComputerName)
+Wait-VIAVMIsRunning -VMname $($ServerData.ComputerName)
+Wait-VIAVMHaveICLoaded -VMname $($ServerData.ComputerName)
+Wait-VIAVMHaveIP -VMname $($ServerData.ComputerName)
+Wait-VIAVMHavePSDirect -VMname $($ServerData.ComputerName) -Credentials $domainCred
+
+#Check that ADDS is up and running
+do{
+$result = Invoke-Command -VMName $($ServerData.ComputerName) -ScriptBlock {
+        Param(
+        $ADDSServerName
+        )
+        Test-Path -Path \\$ADDSServerName\NETLOGON
+    } -Credential $domainCred -ArgumentList $($ServerData.ComputerName)
+    Write-Host "Waiting for Domain Controller to be operational..."
+    Start-Sleep -Seconds 15
+}until($result -eq $true)
+Write-Host "Waiting for Domain Controller is now operational..."
+
+BREAK
+
+Remove-VIAVM -VMName $($ServerData.ComputerName)
---
+++ VeriFyBuildSetup/Verify-DeployServer.ps1
@@ -0,0 +1,82 @@
+﻿#Verify 0.4
+#Make sure this file is located in C:\Setup\HYDV7\Scripts
+
+Param
+(
+    [parameter(position=0,mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({Test-Path -Path $_})]
+    [string]
+    $SettingsFile = "C:\Setup\FABuilds\FASettings.xml",
+
+    [parameter(Position=1,mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({Test-Path -Path $_})]
+    [String]
+    $VHDImage = "C:\Setup\VHD\WS2016-DCE_UEFI.vhdx",
+    
+    [parameter(Position=2,mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({Test-Path -Path $_})]
+    [String]
+    $VMlocation = "D:\VMs"
+)
+
+#Read data from XML
+Write-Verbose "Reading $SettingsFile"
+[xml]$Settings = Get-Content $SettingsFile -ErrorAction Stop
+
+#Import Module
+Import-Module C:\Setup\Functions\VIAHypervModule.psm1 -Force -ErrorAction Stop
+Import-Module C:\Setup\Functions\VIADeployModule.psm1 -Force -ErrorAction Stop
+Import-Module C:\Setup\Functions\VIAUtilityModule.psm1 -Force -ErrorAction Stop
+
+#Get data from XML
+$ServerName = "BUILD01"
+$DomainName = "Fabric"
+
+#Read data from XML
+Write-Verbose "Reading $SettingsFile"
+[xml]$Settings = Get-Content $SettingsFile
+$CustomerData = $Settings.FABRIC.Customers.Customer
+$CommonSettingData = $Settings.FABRIC.CommonSettings.CommonSetting
+$ProductKeysData = $Settings.FABRIC.ProductKeys.ProductKey
+$NetworksData = $Settings.FABRIC.Networks.Network
+$DomainData = $Settings.FABRIC.Domains.Domain | Where-Object -Property Name -EQ -Value $DomainName
+$ServerData = $Settings.FABRIC.BuildServers.BuildServer | Where-Object -Property Name -EQ -Value $ServerName
+
+
+#Verify VMSwitch
+if(!(Test-VIAVMSwitchexistence -VMSwitchname $($CommonSettingData.VMSwitchName))){
+    Write-Warning "The VMSwitch $($CommonSettingData.VMSwitchName) is missing, break"
+    BREAK
+    }else{Write-Host -ForegroundColor Green "Ok."}
+
+
+#Verify VMswitch is not Private
+if((Get-VMSwitch -Name $($CommonSettingData.VMSwitchName)).SwitchType -eq 'Private'){
+    Write-Warning "Switchtype is Private, change to Internal or External, will break"
+    Break
+    }else{Write-Host -ForegroundColor Green "Ok."}
+
+#Check that the build machine will have access to the Fabric Network
+$UplinkSwitchNetAdapter = Get-NetAdapter -Name *$($CommonSettingData.VMSwitchName)*
+$UplinkSwitchNetAdapterSettings = Get-NetIPAddress -InterfaceIndex $UplinkSwitchNetAdapter.ifIndex | Where-Object AddressFamily -EQ IPv4
+
+#Check IP
+if($UplinkSwitchNetAdapterSettings.IPAddress -ne $ServerData.IPAddress){
+    Write-Warning "Your IP on $($UplinkSwitchNetAdapter.Name) is $($UplinkSwitchNetAdapterSettings.IPAddress)"
+    Write-Warning "It should be $($ServerData.IPAddress)"
+    Write-Warning "Will break"
+    Break
+    }else{Write-Host -ForegroundColor Green "Ok."}
+
+#Check PrefixLength
+if($UplinkSwitchNetAdapterSettings.PrefixLength -ne $ServerData.PrefixLength){
+    $CurrentSubnetMask = (Convert-Subnet -PrefixLength $UplinkSwitchNetAdapterSettings.PrefixLength).SubnetMask
+    $ExpectedSubnetMask = (Convert-Subnet -PrefixLength $ServerData.PrefixLength).SubnetMask
+    Write-Warning "Your Subnetmask on $($UplinkSwitchNetAdapter.Name) is $CurrentSubnetMask"
+    Write-Warning "It should be $ExpectedSubnetMask"
+    Write-Warning "Will break"
+    Break
+    }else{Write-Host -ForegroundColor Green "Ok."}
